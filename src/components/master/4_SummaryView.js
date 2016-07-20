5d4560aad31882015e6904b5e88b774f97b95cac
m2wasabi <m2wasabi@gmail.com>
2016-07-20 14:59:18 +0900

    :checkered_flag: Show total results table. :trophy:

--- src/components/master/4_SummaryView.js
+++ src/components/master/4_SummaryView.js
@@ -7,13 +7,30 @@ import { initVote, beginVote, stopVote, showResult, showSummary, showEnding } fr
 
 class SummaryView extends Component {
   render() {
-    const {dispatch} = this.props;
+    const {dispatch, results} = this.props;
+    var turn = 1;
+    var tbody = results.map((result) => {
+      var winner = () => {
+        return((result.team0 > result.team1)? 0:(result.team0 < result.team1)?1:-1);
+      };
+      return ( <tr><th>{turn++} 回戦</th><td className={(winner === 0)?"team0 winner": "team0"}>{result.team0}</td><td className={(winner === 0)?"team1 winner": "team1"}>{result.team0}</td></tr>)
+    });
     return (
       <div>
         <h1>
           累計
         </h1>
         <div>勝敗</div>
+        <div>
+          <table id="tbl-results">
+            <thead>
+            <tr><th>-</th><th className="team0">紅組</th><th className="team1">白組</th></tr>
+            </thead>
+            <tbody>
+            {tbody}
+            </tbody>
+          </table>
+        </div>
         <div>エフェクト</div>
         <button onClick={() => dispatch(showEnding())}>エンディング</button>
       </div>
