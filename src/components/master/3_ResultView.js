54d8f40d3deab63c4de6040807029637d6eba630
m2wasabi <m2wasabi@gmail.com>
2016-07-20 11:20:36 +0900

    :tada: Result view released :tada:

--- src/components/master/3_ResultView.js
+++ src/components/master/3_ResultView.js
@@ -7,27 +7,47 @@ import { initVote, beginVote, stopVote, showResult, showSummary, showEnding } fr
 
 class ResultView extends Component {
   render() {
-    const {dispatch} = this.props;
+    const {dispatch, votes} = this.props;
+    var winner;
+    var message;
+    if(votes.commit.team0 == votes.commit.team1){
+      // 引き分け
+      winner = -1;
+      message = "引き分け";
+    } else if (votes.commit.team0 > votes.commit.team1) {
+      // 紅組の勝ち
+      winner = 0;
+      message = "紅組の勝利！";
+    } else {
+      // 白組の勝ち
+      winner = 1;
+      message = "白組の勝利！";
+    }
     return (
       <div>
         <h1>
           結果
         </h1>
         <div>得票数</div>
+        <div>
+          <ul>
+            <li>紅組<span id="votes-0">{votes.commit.team0}</span></li>
+            <li>白組<span id="votes-1">{votes.commit.team1}</span></li>
+          </ul>
+        </div>
+        <h2>{message}</h2>
         <div>エフェクト</div>
         <button onClick={() => dispatch(initVote())}>戻る</button>
         <button onClick={() => dispatch(showSummary())}>トータル</button>
       </div>
     )
   }
-
 }
 
 function select(state) {
   return {
     stage: state.stage,
-    timeLimit: state.timeLimit,
-    vote: state.votes.commit
+    votes: state.votes
   }
 }
 
