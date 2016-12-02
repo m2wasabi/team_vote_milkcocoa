9e078b7f9fe6c7d569973a3332f67bc42b1a9c40
m2wasabi <m2wasabi@gmail.com>
2016-12-02 17:16:10 +0900

    create new gh-pages

--- app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require('express');
-const http = require('http');
-var app = express();
-app.set('port', process.env.PORT || 8000);
-app.use(express.static('public'));
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
